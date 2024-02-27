e28f212f980d51576b339516d9235a06bd26d23a
SarbaniSarkar <sarbani123sarkar@gmail.com>
2024-02-27 12:00:44 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: true,
+//   }
+  
+//   module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+};
 
 export default nextConfig;
+
